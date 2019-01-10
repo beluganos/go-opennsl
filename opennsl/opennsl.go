68ca335ad94b1969d101cfd3465da0ac29f2e6b5
Shigemasa KUMAGAWA <shigemasa.kumagawa.gw@hco.ntt.co.jp>
2019-01-10 18:49:18 +0900

    first commit

---
+++ opennsl/opennsl.go
@@ -0,0 +1,8 @@
+// -*- coding: utf-8 -*-
+
+package opennsl
+
+const (
+	TRUE  = 1
+	FALSE = 0
+)
---
+++ sal/sal.go
@@ -0,0 +1,11 @@
+// -*- coding: utf-8 -*-
+
+package sal
+
+func DriverInit() error {
+	return nil
+}
+
+func DriverExit() error {
+	return nil
+}
